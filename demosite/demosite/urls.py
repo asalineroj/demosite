f88747de0cf8d9e00688aaa55e174a1d08205e47
AutomatedTester <david.burns@theautomatedtester.co.uk>
2020-04-20 17:26:05 +0100

    Add styling to site

--- demosite/demosite/urls.py
+++ demosite/demosite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/',
          auth_views.LoginView.as_view(template_name='polls/login.html'), name="login"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
