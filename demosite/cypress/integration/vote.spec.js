194e203416c20cdad9454701fd31eb2fb99e1435
andressalinero <andres@browserstack.com>
2021-10-04 21:30:56 +0100

    udpated tests

---
+++ demosite/cypress/integration/vote.spec.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+describe("Authenticated sections", () => {
+    before(() => {
+      cy.fixture("users.json").then((mockedUsers) => {
+  
+        cy.visit("http://localhost:8000/accounts/login/");
+        cy.get("[name=csrfmiddlewaretoken]")
+            .should("exist")
+            .should("have.attr", "value")
+            .as("csrfToken");
+  
+        cy.get("@csrfToken").then((token) => {
+            cy.request({
+            method: "POST",
+            url: "http://localhost:8000/accounts/login/", 
+            form: true,
+            body: {
+                username: mockedUsers[0].username,
+                password: mockedUsers[0].password,
+            },
+            headers: {
+                "X-CSRFTOKEN": token,
+            },
+            });
+        });
+      });
+  
+      cy.getCookie("sessionid").should("exist");
+      cy.getCookie("csrftoken").should("exist");
+    });
+  
+    beforeEach(() => {
+      Cypress.Cookies.preserveOnce("sessionid", "csrftoken");
+    });
+
+    it("Can visit List view through the UI", function () {
+        cy.visit("http://localhost:8000/polls/1/");
+        cy.get('#choice1').should("be.visible")
+        cy.percysnapshot("Poll 1")
+        cy.get('#choice1').click()
+    });
+
+    it("Can visit List view through the UI", function () {
+        cy.visit("http://localhost:8000/polls/2/");
+        cy.get('#choice1').should("be.visible")
+        cy.percysnapshot("Poll 2")
+        cy.get('#choice1').click()
+    });
+
+})
