8235715b4500c33464bf2f4f017e7bea53010f74
andressalinero <andres@browserstack.com>
2021-10-04 19:28:49 +0100

    added percy snapshots

--- demosite/cypress/integration/authentication.spec.js
+++ demosite/cypress/integration/authentication.spec.js
@@ -7,6 +7,8 @@ describe("Login", () => {
   
     it("Can login through the UI", function () {
       cy.visit("http://localhost:8000/accounts/login");
+      cy.get("input[name='username']").should("be.visible")
+      cy.percySnapshot("Login Form");
       cy.get("input[name='username']").type(this.mockedUsers[0].username);
       cy.get("input[name='password']").type(this.mockedUsers[0].password);
       cy.get("form").submit();
@@ -18,6 +20,9 @@ describe("Login", () => {
         cy.get("input[name='username']").type(this.mockedUsers[1].username);
         cy.get("input[name='password']").type(this.mockedUsers[1].password);
         cy.get("form").submit();
+        cy.get("p").contains("Your username and password didn't match. Please try again.").should("be.visible")
+        cy.percySnapshot('Login Wrong Password')
         cy.getCookie("sessionid").should("not.exist");
       });
+
   });
